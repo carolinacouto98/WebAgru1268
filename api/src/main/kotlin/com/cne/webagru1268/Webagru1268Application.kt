8a44d17a27791217c688a651cf6d708e12ce0406
carolinacouto98 <carolinacouto13@hotmail.com>
2022-01-27 00:05:49 +0000

    Created api spring application

---
+++ api/src/main/kotlin/com/cne/webagru1268/Webagru1268Application.kt
@@ -0,0 +1,11 @@
+package com.cne.webagru1268
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Webagru1268Application
+
+fun main(args: Array<String>) {
+	runApplication<Webagru1268Application>(*args)
+}
---
+++ api/src/test/kotlin/com/cne/webagru1268/Webagru1268ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cne.webagru1268
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Webagru1268ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
